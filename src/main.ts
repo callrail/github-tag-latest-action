73899b79c74b773f8b556e18bb41d4fcc850a342
Dan Smith <dan@callrail.com>
2021-02-10 11:09:25 -0500

    Wire up checking for success status and tagging with 'latest'

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,38 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {context, getOctokit} from '@actions/github'
+
+const latestTagRef = 'latest'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const token: string = core.getInput('github_token')
+    const octokit = getOctokit(token)
+    const {repo, sha} = context
+    const {data: combinedStatus} = await octokit.repos.getCombinedStatusForRef({
+      ...repo,
+      ref: sha
+    })
+    core.debug(`Status state for sha ${sha} is ${combinedStatus.state}.`)
+    if (combinedStatus.state === 'success') {
+      const tagsList = await octokit.repos.listTags({
+        ...repo,
+        per_page: 100
+      })
+      const {data: tags} = tagsList
+      const latestTag = tags.find(tag => tag.name === latestTagRef)
+      if (latestTag) {
+        octokit.git.deleteRef({
+          ...repo,
+          ref: latestTagRef
+        })
+      }
+      octokit.git.createRef({
+        ...repo,
+        ref: latestTagRef,
+        sha
+      })
+      core.debug(`Commit ${sha} tagged with 'latest'`)
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
