5a6d45719cdf1c76f889e241abaa1212f47e94fe
Dan Smith <dan@callrail.com>
2021-02-11 14:11:03 -0500

    Don't try to run job if status isn't success

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import * as core from '@actions/core'
 import {Actions} from './action'
+import {Octokit} from './octokit'
 
 async function run(): Promise<boolean | undefined> {
   try {
-    return new Actions().updateLatestTag().toPromise()
+    const wrapper = new Octokit()
+    const success = await wrapper.stateIsSuccess().toPromise()
+    if (success) {
+      core.debug('State for status is success!')
+      return new Actions().updateLatestTag().toPromise()
+    }
+    core.debug('State was not success yet')
   } catch (error) {
     core.setFailed(error.message)
   }
